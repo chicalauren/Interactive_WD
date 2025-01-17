97eed8e8d6053eaf7d2e589eebaae409017481dd
chicalauren <lambertelauren@gmail.com>
2025-01-16 19:44:23 -0600

    Updated script.js, style.css, and index.html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
+const openPopupButton = document.getElementById("openPopup");
+const closePopupButton = document.getElementById("closePopup");
+const popup = document.getElementById("popup");
 
+openPopupButton.addEventListener("click", () => {
+  popup.classList.remove("hidden");
+});
+
+closePopupButton.addEventListener("click", () => {
+  popup.classList.add("hidden");
+});
